f80c40d516b624cd544944ab0bf6d63e5c2745d0
SPEED <rqmtw4058@gmail.com>
2025-01-24 11:32:11 +0200

    Update intl.bat

--- intl.bat
+++ intl.bat
@@ -1,10 +1,10 @@
 @echo off
-REM Change Windows language to English (United States)
+REM Change Windows language to English (United States) for Windows 10/11
 
 echo Installing English (United States) language pack...
 dism /online /add-package /packagepath:C:\Windows\LanguagePack\en-us\lp.cab >nul 2>&1
 
-REM Set language settings
+REM Set English (United States) as the default language
 echo Setting English (United States) as the default language...
 powershell -Command "Set-WinUILanguageOverride -Language en-US"
 powershell -Command "Set-WinUserLanguageList -LanguageList en-US -Force"
@@ -12,12 +12,10 @@ powershell -Command "Set-WinSystemLocale en-US"
 powershell -Command "Set-Culture en-US"
 powershell -Command "Set-WinHomeLocation -GeoId 244"
 
-REM Automatically copy settings to Welcome screen and System accounts
+REM Copy language settings to Welcome screen and system accounts
 echo Applying language settings to Welcome screen and system accounts...
-reg add "HKEY_CURRENT_USER\Control Panel\International\Geo" /v Nation /t REG_SZ /d 244 /f
-reg add "HKEY_CURRENT_USER\Control Panel\International" /v Locale /t REG_SZ /d 00000409 /f
-reg add "HKEY_USERS\.DEFAULT\Control Panel\International" /v Locale /t REG_SZ /d 00000409 /f
+control intl.cpl,, /f:"%~dp0intl.xml"
 
-REM Notify user
-echo Language settings applied. Please restart your computer to apply changes.
+REM Notify user to restart
+echo Language settings applied successfully. Please restart your computer to apply changes.
 pause
